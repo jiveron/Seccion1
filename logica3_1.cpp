21d3c59b3767f05291cee014cde14a9f08cb1d7e
diego-rz <diegorzuleta@gmail.com>
2016-08-03 11:34:03 -0300

    Agregada funcion de verificacion

--- logica3_1.cpp
+++ logica3_1.cpp
@@ -8,20 +8,33 @@
 using namespace std;
 
 int main(){
-  bool P, S, T, J;
-  cout << "Dada la siguiente proposicion:\n\n(P^S^T)v(¬S^¬P^J)\n\nDar los valores de verdad de las proposiciones simples(1=verdadero ; 0=falso):\n\nP: ";
-  cin >> P;
-  cout << "S: ";
-  cin >> S;
-  cout << "T: ";
-  cin >> T;
-  cout << "J: ";
-  cin >> J;
+  short P, S, T, J;
+  bool verificar(short);
 
-  if( (P and S and T) or (!S and !P and J) ){
+  cout << "Dada la siguiente proposicion:\n\n(P^S^T)v(¬S^¬P^J)\n\nDar los valores de verdad de las proposiciones simples(1=verdadero , 0=falso):\n\n";
+  cout << "P: "; cin >> P;
+  if(!verificar(P)) {cout<<"Error\n"; return 1;}
+
+  cout << "S: "; cin >> S;
+  if(!verificar(S)) {cout<<"Error\n"; return 1;}
+
+  cout << "T: "; cin >> T;
+  if(!verificar(T)) {cout<<"Error\n"; return 1;}
+
+  cout << "J: "; cin >> J;
+  if(!verificar(J)) {cout<<"Error\n"; return 1;}
+
+
+  if( (P and S and T) or (!S and !P and J) )
     cout << "\nLa proposicion es verdadera.\n\n";
-  }
-  else{
+  else
     cout << "\nLa proposicion es falsa.\n\n";
-  }
+}
+
+// funcion que devuleve verdadero si se ingreso 1 o 0, y falso en cualquier otro caso
+bool verificar(short a){
+  if(a<0 or a>1)
+    return false;
+  else
+    return true;
 }
